5ccb639b25a04ab593bc3c6d60c9fef9df486c94
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-04-05 19:40:04 -0400

    feat: response streaming

--- python/oogway/demo.py
+++ python/oogway/demo.py
@@ -1,10 +1,20 @@
 import asyncio
+import sys
+import time
 import oogway
 
 
 ai = oogway.Oogway()
 ai.model_name = "gpt-3.5-turbo"
-async def query():
-    await ai.ask("Why?")
-res = query()
-print(res)
+
+async def talk_to_oogway(question: str):
+    print(f"\n> You :\n\t{question}");
+    while True:
+        print("\n> Oogway : \n\t", end="");
+        async for chunk in ai.ask(question):
+            sys.stdout.write(chunk)
+            sys.stdout.flush()
+        question = input("\n> You: \t ")
+
+if __name__ == "__main__":
+    asyncio.run(talk_to_oogway("why?"))
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-use std::time::Duration;
+use std::{pin::Pin, sync::Arc};
 
-use ::oogway::Oogway as Oogwayy;
-use pyo3::{exceptions::PyKeyError, prelude::*};
+use ::oogway::{ask_helper, Oogway as _Oogway};
+use async_openai::{error::OpenAIError, types::CreateChatCompletionStreamResponse};
+use async_std::stream::{IntoStream, StreamExt};
+use futures::Stream;
+use pyo3::{
+    exceptions::{PyKeyError, PyStopAsyncIteration},
+    pyclass, pymethods, pymodule,
+    types::PyModule,
+    PyObject, PyRef, PyResult, Python,
+};
+use tokio::sync::Mutex;
+
+// use pyo3::{exceptions::PyKeyError, prelude::*};
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -10,45 +21,70 @@ fn oogway(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-
 // not really optimized , prolly a better way to do this
 #[pyclass]
+#[derive(Clone)]
 pub struct Oogway {
-    inner: Oogwayy
+    inner: _Oogway,
 }
+
+// https://github.com/awestlake87/pyo3-asyncio/issues/17
 
 #[pymethods]
 impl Oogway {
     #[new]
     fn new() -> PyResult<Oogway> {
-        match Oogwayy::new() {
-            Ok(oogway) => Ok(Self{inner : oogway }),
+        match _Oogway::new() {
+            Ok(oogway) => Ok(Self { inner: oogway }),
             Err(e) => Err(PyKeyError::new_err(e.to_string())),
         }
     }
 
     #[setter(model_name)]
-    fn set_model_name(&mut self, model_name: String){
+    fn set_model_name(&mut self, model_name: String) {
         self.inner.model(model_name);
     }
 
-    pub fn ask<'a >(&mut self, py: Python<'a>, question: String) -> PyResult<&'a PyAny> {
-        let x = self.inner.ask(question);
-        // x
-        pyo3_asyncio::async_std::into_coroutine(py, async move {
-            async_std::task::sleep(Duration::from_secs(1)).await;
-            Ok(())
+    // &PyAny
+    pub fn ask(&mut self, _py: Python, question: String) -> PyResult<RespStream> {
+        let i = self.inner.clone();
+        let stream = pyo3_asyncio::tokio::get_runtime()
+            .block_on(async { ask_helper(i, question).await.unwrap().into_stream() });
+        Ok(RespStream {
+            s: Arc::new(Mutex::new(stream)),
         })
-        // pyo3_asyncio::tokio::future_into_py(py, async move {
-        //     async_std::task::sleep(std::time::Duration::from_secs(1)).await;
-        //     Ok(Python::with_gil(|py| py.None()))
-        // })
+    }
+}
 
-        // pyo3_asyncio::async_std::into_coroutine(py, async move {
-        //     let x = self.inner.ask(question);
-        //     let x = x.await;
-        //     Ok(())
-        // })
+
+type S =  Arc<Mutex<Pin<Box<dyn Stream<Item = Result<CreateChatCompletionStreamResponse, OpenAIError>>+ Send>>>>;
+
+#[pyo3::pyclass]
+pub struct RespStream {
+    s: S,
+}
+
+#[pymethods]
+impl RespStream {
+    fn __aiter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
     }
 
+    fn __anext__(&self, py: Python) -> PyResult<Option<PyObject>> {
+        let f = self.s.clone();
+        let future = pyo3_asyncio::tokio::future_into_py(py, async move {
+            let val = f.lock().await.next().await;
+            match val {
+                Some(val) => Ok(val
+                    .unwrap()
+                    .choices
+                    .iter()
+                    .filter_map(|chat_choice| chat_choice.delta.content.clone())
+                    .collect::<String>()),
+                None => Err(PyStopAsyncIteration::new_err("The iterator is exhausted")),
+            }
+        });
+
+        Ok(Some(future?.into()))
+    }
 }
